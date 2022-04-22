0af3a6d9c685f058678212edcda365e7f0a0fbaf
MonikaBoom <53166547+MonikaBoom@users.noreply.github.com>
2022-04-22 12:10:52 +0530

    Files Uploaded
    Files commit

---
+++ next.config.js
@@ -0,0 +1,37 @@
+const webpack = require('webpack');
+const withPWA = require('next-pwa')
+const isProd = process.env.NODE_ENV === 'production'
+
+const nextConfig = withPWA(
+  {
+    assetPrefix: isProd && process.env.CDN_URL_FOR_STATIC_RESOURCES ? process.env.CDN_URL_FOR_STATIC_RESOURCES : '',
+    publicRuntimeConfig: {
+      localeSubpaths: typeof process.env.LOCALE_SUBPATHS === 'string'
+        ? process.env.LOCALE_SUBPATHS
+        : 'none',
+    },
+    eslint: {
+      // Warning: This allows production builds to successfully complete even if
+      // your project has ESLint errors.
+      ignoreDuringBuilds: true,
+    },
+    basePath:process.env.subFolder.slice(0, -1),
+    webpack5: true,
+    webpack: function (config) {
+      config.plugins.push(new webpack.ProvidePlugin({
+        $: 'jquery',
+        jQuery: 'jquery',
+        'window.jQuery': 'jquery'
+      }))
+      return config
+    },
+    pwa: {
+      disable: process.env.NODE_ENV === 'production' ? false : true,
+      dest: "public",
+      register: false,
+    },
+    poweredByHeader:false,
+  }
+);
+
+module.exports = nextConfig
